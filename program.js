35956485391958c98156e97ae4414f94826581e4
natarodri <natharhernandez17@gmail.com>
2025-03-06 21:16:40 -0500

    v0.2

---
+++ program.js
@@ -0,0 +1,3 @@
+document.getElementById("nombre-barrio").innerText = "SAN MARTIN";
+document.getElementById("localidad").innerText = "SAN CRISTOBAL";
+document.getElementById("foto-barrio").src = "SAN MARTIN.jpg";  // Cambia por la imagen correcta
